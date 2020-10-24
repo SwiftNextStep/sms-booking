48d19934058c249b8a8a64146ddfdff023354f94
Icaro <icaro@Icaros-iMac.local>
2020-10-24 13:18:18 +1300

    Setup express and Nodemon

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,4 +1,20 @@
+const { response } = require('express');
+
 require('dotenv').config();
+
+const server = require('express')();
+
+const port = process.env.EXPRESS_PORT || 3001;
+
+server.get('/test', (request, response) => {
+  response.send('Hello from nodemon started with the script');
+});
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+const from = process.env.PHONE_NUMBER;
+const to = process.env.MY_NUMBER;
 
 const twilio = require('twilio')(
   process.env.TOKEN_SID,
@@ -7,3 +23,14 @@ const twilio = require('twilio')(
     accountSid: process.env.ACCOUNT_SID,
   }
 );
+
+function sendSms() {
+  twilio.messages
+    .create({
+      from,
+      to,
+      body: 'Hello from Twilio',
+    })
+    .then((message) => console.log(`Message sent with sid ${message.sid}`))
+    .catch((error) => console.error(error));
+}
